b71a25d7c52785ffa3554131e0cb43973cba5bd1
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-04-05 20:59:01 +0100

    update on Task 2

--- Newton.m
+++ Newton.m
@@ -4,17 +4,17 @@
 % N0 (max. iterations)
 % Output: the value p
 function p = Newton(f, df, p0, TOL, N0)
-fprintf('%3d:%16.9f\n', 0, p0); 
+%fprintf('%3d:%16.9f\n', 0, p0); 
 %Step 1:
 i = 1;
 %Step 2:
 while i <= N0
    %Step 3:
    p = p0 - f(p0)/df(p0);
-   fprintf('%3d:%16.9f\n', i, p); 
+   %fprintf('%3d:%16.9f\n', i, p); 
    %Step 4:
    if abs(p - p0) < TOL
-       fprintf('Solution found p = %g\n', p);
+       %fprintf('Solution found p = %g\n', p);
        return
    end
    %Step 5:
@@ -22,6 +22,6 @@ while i <= N0
    %Step 6:
    p0 = p;  
 end
-fprintf('Method failed after %d iterations\n', N0);
+%fprintf('Method failed after %d iterations\n', N0);
 
 end
--- visualiseConvergence1.m
+++ visualiseConvergence1.m
@@ -1,8 +1,35 @@
 % pass function as parameter
-function visualiseConvergence1(f)
+function visualiseConvergence1(y)
     % create matrix for storing values
+    syms f(x);
+    f(x) = y;
+    %dataX = -10:0.02:10; % stores values in required range at evenly spaced increments
+    dataX = -5:1:5;
+    dataY = zeros(1000); % creates empty matrix
+    firstBad = NaN;
+    df = matlabFunction(diff(f));
+    
+    % reduced from 1000 to 10 for testing purposes
+    for i = 1:10
+        % do this for each value in dataX
+        
+        dataY(i) = f(dataX(i));
+        %result = Newton(f, df, dataX(i), 0.00001, 100);
+        result = Newton(f, df, 0, 0.00001, 100);
 
+        if isnan(result)
+            % if NaN Newton method failed
+            % do nothing
+        elseif isnan(firstBad)
+            % do nothing, result has failed but first bad already recorded
+        else
+            firstBad = i;
+        end
+        
+    end
     
-    dataX = -10:0.02:10; % stores values in required rangeat evenly spaced increments
-    
+    plot(dataX(1:10), dataY(1:10), "-b");
+    hold on;
+    %plot(dataX(firstBad+1 : 1000), dataY(firstBad+1:1000), "-r");
+end
     
