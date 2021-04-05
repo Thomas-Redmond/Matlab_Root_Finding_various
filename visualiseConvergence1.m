c673ca07052c082ef9bd089fc40ec00b9a3681e2
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-04-05 19:45:54 +0100

    moved files + added ignore to ignore temp files

---
+++ visualiseConvergence1.m
@@ -0,0 +1,8 @@
+% pass function as parameter
+function visualiseConvergence1(f)
+    % create matrix for storing values
+
+    
+    dataX = -10:0.02:10; % stores values in required rangeat evenly spaced increments
+    
+    
