955b38878b588f80e5c1d01ea2841f297403611a
Thomas Lea-Redmond <Lea-RedmondT@cardiff.ac.uk>
2021-04-06 10:50:35 +0100

    implementing Newton full implementation attempt 2

--- visualiseConvergence1.m
+++ visualiseConvergence1.m
@@ -3,14 +3,14 @@ function visualiseConvergence1(y)
     % create matrix for storing values
     syms f(x);
     f(x) = y;
-    %dataX = -10:0.02:10; % stores values in required range at evenly spaced increments
-    dataX = -5:1:5;
+    dataX = -10:0.02:10; % stores values in required range at evenly spaced increments
+    %dataX = -5:1:5;
     dataY = zeros(1000); % creates empty matrix
-    firstBad = NaN;
+    firstBad = 1001;        % set to 1001 as outside limit but 
     df = matlabFunction(diff(f));
     
-    % reduced from 1000 to 10 for testing purposes
-    for i = 1:10
+    
+    for i = 1:1000
         % do this for each value in dataX
         
         dataY(i) = f(dataX(i));
@@ -23,13 +23,16 @@ function visualiseConvergence1(y)
         elseif isnan(firstBad)
             % do nothing, result has failed but first bad already recorded
         else
-            firstBad = i;
+            firstBad = i; % first point where position does not lead to convergence
         end
-        
+        fprintf('Iteration : %d.\n', i);
+        %disp('Iteration: ' i '\n');
     end
     
-    plot(dataX(1:10), dataY(1:10), "-b");
+    plot(dataX(1:firstBad-1), dataY(1:firstBad-1), "-b"); % data that converges plots in blue
     hold on;
-    %plot(dataX(firstBad+1 : 1000), dataY(firstBad+1:1000), "-r");
+    if firstBad <= 1000
+        plot(dataX(firstBad : 1000), dataY(firstBad:1000), "-r"); % data that does not converge in red
+    end
 end
     
